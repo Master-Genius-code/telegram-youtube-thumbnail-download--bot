3ca4b79acfea213d3dff5f82aabd8862a1b7fead
Master-Genius-code <74366880+Master-Genius-code@users.noreply.github.com>
2021-04-01 17:56:30 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,45 @@
+from bot import telegram_chatbot
+from urllib.parse import urlparse, parse_qs
+from urllib import request
+import json
+
+bot = telegram_chatbot("config.cfg")
+
+def valid_url(e):
+    try:
+        url_ = "https://www.youtube.com/oembed?format=json&url=" + e
+        response = request.urlopen(url_)
+        page_source = response.read()
+        str_ = str(page_source.decode('utf-8'))
+        dict_ = json.loads(str_)
+        return (dict_['thumbnail_url'])
+    except:
+        return None
+
+def make_reply(msg,chat_id):
+    
+    if msg is not None:
+        if msg == '/start':
+            bot.send_message('Welcome to Youtube Thumbnail Download bot . Send a youtube video url ',chat_id)
+        else:
+            if valid_url(msg) is not None:
+                    thumbnailurl= valid_url(msg)
+                    bot.sendImageRemoteFile(chat_id,thumbnailurl)
+            else:
+                bot.send_message('Please Enter a valid youtube video url',chat_id)
+
+update_id = None
+
+while True:
+    updates = bot.get_updates(offset=update_id)
+    
+    updates = updates["result"]
+    if updates:
+        for item in updates:
+            update_id = item["update_id"]
+            try:
+                message = str(item["message"]["text"])
+            except:
+                message = None
+            from_ = item["message"]["from"]["id"]
+            make_reply(message,from_)
